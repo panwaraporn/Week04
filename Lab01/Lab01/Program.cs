3a2d2e93a3d367bb3abbf6e1d9c6d91d37db7500
panwaraporn <58030219@kmitl.ac.th>
2017-10-24 20:48:06 +0700

    commit branch Lab03P15-6

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -12,6 +12,7 @@ namespace RandomNumber
             int Num = Convert.ToInt32(Console.ReadLine());
             bool a = Num > randomNumber;
             Console.WriteLine("Truth is : {0} > {1} ", Num, randomNumber);
+            Console.WriteLine("Truth is : {0}", a);
         }
     }
 }
