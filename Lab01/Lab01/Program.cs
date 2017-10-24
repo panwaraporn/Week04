d7c6fc909233061ef1c986af2642248ac55d84b7
panwaraporn <58030219@kmitl.ac.th>
2017-10-24 21:47:32 +0700

    commit branch Lab03P19

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,29 +1,19 @@
 ﻿using System;
-public class intergerTest
+namespace variableProperties
 {
-    static void Main(string[] args)
+    class Program
     {
-        int a = 10, b = 20, x = 5, y = 2;
-        Console.WriteLine("a = 10, b = 20, x = 5, y = 2 ");
-        int answer1 = a + b;
-        Console.WriteLine("a + b={0} ", answer1);
-        int answer2 = x - b;
-        Console.WriteLine("x - b={0} ", answer2);
-        int answer3 = x * b;
-        Console.WriteLine("x * b={0} ", answer3);
-        int answer4 = y / a;
-        Console.WriteLine("y / a={0} ", answer4);
-        int answer5 = b % y;
-        Console.WriteLine("b % y={0} ", answer5);
-        int answer6 = y + 10 % x;
-        Console.WriteLine("y + 10 % x={0} ", answer6);
-        int answer7 = a / 3 * 5;
-        Console.WriteLine("a / 3 * 5={0} ", answer7);
-        int answer8 = 9 / 2 * a;
-        Console.WriteLine("9 / 2 * a={0} ", answer8);
-        int answer9 = y % 8;
-        Console.WriteLine("y % 8={0} ", answer9);
-        int answer10 = 100 * x + y % 2 - a;
-        Console.WriteLine("100 * x + y % 2 - a={0} ", answer10);
+        static void Main(string[] args)
+        {
+            const double lightSpeed = 186000d;   // miles per second
+            Console.WriteLine("Light speed = {0} Mile Per second", lightSpeed);
+            const double mileTokm = 1.609344;
+            Console.WriteLine("Light speed = {0} km Per second", lightSpeed * mileTokm);
+            const double SunToEarthDistance = 93000000d;  // miles
+            Console.WriteLine("SunToEarthDistance = {0} km", SunToEarthDistance * mileTokm);
+            double SunToEarthTimeOfLight = SunToEarthDistance / lightSpeed;  // miles
+            Console.WriteLine("SunToEarthTimeOfLight = {0} seconds", SunToEarthTimeOfLight);
+            Console.WriteLine("SunToEarthTimeOfLight = {0} minutes", SunToEarthTimeOfLight / 60d);
+        }
     }
 }
