e7a304c1a1ed6b2a4e1838877c206b5f2a310994
panwaraporn <58030219@kmitl.ac.th>
2017-10-24 20:47:06 +0700

    commit branch Lab03P15-5

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,9 +5,13 @@ namespace RandomNumber
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 9);
+            Console.WriteLine(randomNumber);
             Console.Write("Your number key:");
             int Num = Convert.ToInt32(Console.ReadLine());
-
+            bool a = Num > randomNumber;
+            Console.WriteLine("Truth is : {0} > {1} ", Num, randomNumber);
         }
     }
 }
