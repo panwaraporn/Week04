de53c31cb86a6185802375f772bb9d540129ec6a
panwaraporn <58030219@kmitl.ac.th>
2017-10-24 21:56:08 +0700

    commit branch Lab03P20-2

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -3,15 +3,15 @@ public class MathTest
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 11; i += 1)
+        for (float i = 0; i < Math.PI * 2.0F; i += 0.3F)
         {
-            Console.WriteLine("The X*X of {0,10:G} = {1,-10:G}" +
-            spaces(Math.Abs(i * i)) + "*", i, Math.Abs(i * i));
+            Console.WriteLine("The Cos of {0,10:F} = {1,-10:F6}" +
+            spaces(Math.Cos(i)) + "*", i, Math.Cos(i));
         }
     }
     private static string spaces(double val)
     {
-        string SpaceString = new String(' ', ((int)(val * 2)) + 2);
+        string SpaceString = new String(' ', ((int)(val * 10.0)) + 10);
         return SpaceString;
     }
 }
